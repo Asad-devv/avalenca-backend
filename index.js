2997bf201b4ce4154dd88086725cb8e3078a0661
asad <shaikhasad183@gmail.com>
2024-05-07 20:27:00 +0500

    3rd

--- index.js
+++ index.js
@@ -6,19 +6,24 @@ const app = express();
 const port = 3001;
 
 // MySQL database connection configuration
-// MySQL database connection configuration
-const db = mysql.createPool({
-  host: "193.203.175.59", // Use the MySQL server hostname or IP address
+const db2 = mysql.createPool({
+  host: "srv1198.hstgr.io", // Use the MySQL server hostname or IP address
   user: "u199104033_usdt3", // Your MySQL username
   password: "Alcavenca123", // Your MySQL password
   database: "u199104033_usdt3", // Your MySQL database name
+});
+const db = mysql.createPool({
+  host: "srv1198.hstgr.io", // Use the MySQL server hostname or IP address
+  user: "u199104033_usdt1", // Your MySQL username
+  password: "Alcavenca123", // Your MySQL password
+  database: "u199104033_usdt1", // Your MySQL database name
 });
 
 // Connect to MySQL database
 
 db.getConnection((err, connection) => {
   if (err) {
-    console.error("Error connecting to MySQL:", err);
+    console.error(" Error connecting to MySQL:", err);
     process.exit(1); // Exit the application if the connection fails
   }
   console.log("Connected to MySQL database");
@@ -120,6 +125,23 @@ app.post('/saveData', async (req, res) => {
             Conservación_y_preservación_de_los_productos_2, Picking_productos_1, Picking_productos_2]
         );
         
+
+        await db2.query(
+          "INSERT INTO formdata " +
+          "(name, country, city, email, wallet, formtype, paymentAmount, Logros, Concretados_fase_1, fase_1, Concretados, fase_2, Concretados_fase_2, " +
+          "por_llamar1, er_llamado1, por_llamar2, llamado_2do, Total_Administrativo, Pendientes, Salidas, Ingresos, zona_1, zona_2, total_zona_1, total_zona_2, " +
+          "Contratacion_1, Contratacion_2, Conflictos_1, Conflictos_2, Capacitacion_1, Capacitacion_2, Mayorista_1, Mayorista_2, Minorista_1, Minorista_2, " +
+          "Comisionista_1, Comisionista_2, Dist_Zona_1, Dist_Zona_2, Enlace_1, Enlace_2, prod_er_Nivel1, prod_do_Nivel2, Pronóstico_1, Pronóstico_2, " +
+          "Adquisición_Productos_1, Adquisición_Productos_2, Inventario_1, Inventario_2, Almacenamiento_1, Almacenamiento_2, Conservación_y_preservación_de_los_productos_1, " +
+          "Conservación_y_preservación_de_los_productos_2, Picking_productos_1, Picking_productos_2) " +
+          "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+          [name, country, city, email, wallet, formtype, paymentAmount, Logros, Concretados_fase_1, fase_1, Concretados, fase_2, Concretados_fase_2,
+          por_llamar1, er_llamado1, por_llamar2, llamado_2do, Total_Administrativo, Pendientes, Salidas, Ingresos, zona_1, zona_2, total_zona_1, total_zona_2,
+          Contratacion_1, Contratacion_2, Conflictos_1, Conflictos_2, Capacitacion_1, Capacitacion_2, Mayorista_1, Mayorista_2, Minorista_1, Minorista_2,
+          Comisionista_1, Comisionista_2, Dist_Zona_1, Dist_Zona_2, Enlace_1, Enlace_2, prod_er_Nivel1, prod_do_Nivel2, Pronóstico_1, Pronóstico_2,
+          Adquisición_Productos_1, Adquisición_Productos_2, Inventario_1, Inventario_2, Almacenamiento_1, Almacenamiento_2, Conservación_y_preservación_de_los_productos_1,
+          Conservación_y_preservación_de_los_productos_2, Picking_productos_1, Picking_productos_2]
+      );
         // Destructure mgmtData fields
         // Destructure mgmtData fields
         const {
@@ -202,6 +224,31 @@ app.post('/saveData', async (req, res) => {
             Dirección_Generalestrategias_1, Dirección_Generalestrategias_2, Dirección_Generalestrategias_3, Dirección_Generalestrategias_4,
             Dirección_Generalestrategias_5, Dirección_Generalestrategias_6, politicas_nuevas_Seleccionar]
         );
+
+        await db2.query(
+          "INSERT INTO managementdata " +
+          "(Metas_alcanzar_org1, Seleccionar1_org1, Seleccionar2_org1, Seleccionar3_org1, Seleccionar4_org1, Imposiciones_org1, " +
+          "Metas_alcanzar_org2, Seleccionar1_org2, Seleccionar2_org2, Seleccionar3_org2, Seleccionar4_org2, Imposiciones_org2, " +
+          "Metas_alcanzar_org3, Seleccionar1_org3, Seleccionar2_org3, Seleccionar3_org3, Seleccionar4_org3, Imposiciones_org3, " +
+          "Metas_alcanzar_org4, Seleccionar1_org4, Seleccionar2_org4, Seleccionar3_org4, Seleccionar4_org4, Imposiciones_org4, " +
+          "Control_de_Gestión, ControlGestión1, ControlGestión2, ControlGestión3, ControlGestión4, Asistencia_a_la_dirección, " +
+          "Estructura_contable, selection_Estructura_1, selection_Estructura_2, selection_Estructura_3, selection_Estructura_4, " +
+          "Coordinación_y_optimización, transacciones_Seleccionar, Servicio_al_Cliente_1, Servicio_al_Cliente_2, Servicio_al_Cliente_3, " +
+          "Servicio_al_Cliente_4, Servicio_al_Cliente_5, Servicio_al_Cliente_6, problemas_resueltos_Seleccionar, Reuniones_con_zonas_Seleccionar, " +
+          "Dirección_Generalestrategias_1, Dirección_Generalestrategias_2, Dirección_Generalestrategias_3, Dirección_Generalestrategias_4, " +
+          "Dirección_Generalestrategias_5, Dirección_Generalestrategias_6, politicas_nuevas_Seleccionar) " +
+          "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+          [Metas_alcanzar_org1, Seleccionar1_org1, Seleccionar2_org1, Seleccionar3_org1, Seleccionar4_org1, Imposiciones_org1,
+          Metas_alcanzar_org2, Seleccionar1_org2, Seleccionar2_org2, Seleccionar3_org2, Seleccionar4_org2, Imposiciones_org2,
+          Metas_alcanzar_org3, Seleccionar1_org3, Seleccionar2_org3, Seleccionar3_org3, Seleccionar4_org3, Imposiciones_org3,
+          Metas_alcanzar_org4, Seleccionar1_org4, Seleccionar2_org4, Seleccionar3_org4, Seleccionar4_org4, Imposiciones_org4,
+          Control_de_Gestión, ControlGestión1, ControlGestión2, ControlGestión3, ControlGestión4, Asistencia_a_la_dirección,
+          Estructura_contable, selection_Estructura_1, selection_Estructura_2, selection_Estructura_3, selection_Estructura_4,
+          Coordinación_y_optimización, transacciones_Seleccionar, Servicio_al_Cliente_1, Servicio_al_Cliente_2, Servicio_al_Cliente_3,
+          Servicio_al_Cliente_4, Servicio_al_Cliente_5, Servicio_al_Cliente_6, problemas_resueltos_Seleccionar, Reuniones_con_zonas_Seleccionar,
+          Dirección_Generalestrategias_1, Dirección_Generalestrategias_2, Dirección_Generalestrategias_3, Dirección_Generalestrategias_4,
+          Dirección_Generalestrategias_5, Dirección_Generalestrategias_6, politicas_nuevas_Seleccionar]
+      );
         
         console.log('Data inserted into both tables successfully');
         res.status(200).json({ message: 'Data saved successfully' });
@@ -213,27 +260,31 @@ app.post('/saveData', async (req, res) => {
 
 
 app.get("/tables", async (req, res) => {
-    try {
-      const formType = req.query.formType; // Extract formType from query parameters
-  
-      let sql;
-      if (formType === "form1") {
-        sql =
-          "SELECT name, country, wallet, paymentAmount FROM formdata WHERE formtype ='form1' LIMIT 50";
-      } else if (formType === "form2") {
-        sql =
-          "SELECT name, country, wallet, paymentAmount FROM formdata WHERE formtype ='form2' LIMIT 50";
-      } else {
-        throw new Error("Invalid form type");
-      }
-  
-      const [results] = await db.query(sql);
-      res.status(200).json(results);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-      console.log(error);
+  try {
+    const formType = req.query.formType; // Extract formType from query parameters
+
+    let sql;
+    let dbConnection;
+    if (formType === "1") {
+      sql =
+        "SELECT name, country, wallet, paymentAmount FROM db1.formdata WHERE formtype ='form1' LIMIT 50";
+      dbConnection = db;
+    } else if (formType === "3") {
+      sql =
+        "SELECT name, country, wallet, paymentAmount FROM db2.formdata WHERE formtype ='form2' LIMIT 50";
+      dbConnection = db2;
+    } else {
+      throw new Error("Invalid form type");
     }
-  });
+
+    const [results] = await dbConnection.query(sql);
+    res.status(200).json(results);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log(error);
+  }
+});
+
   
 
 // Route to display the port on which the server is running
@@ -250,7 +301,7 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "app is running" });
 });
 
-app.post('/create-columns', (req, res) => {
+app.post('/create-columns', async (req, res) => {
   // Get the payload from the request
   const payload = req.body;
 
@@ -263,23 +314,20 @@ app.post('/create-columns', (req, res) => {
       }
     }
     columnDefinitions = columnDefinitions.slice(0, -1); // Remove trailing comma
-    const sql = `ALTER TABLE formdata ADD (${columnDefinitions})`;
+    const sql = `ALTER TABLE management_data ADD (${columnDefinitions})`;
     return sql;
   }
 
   // Get SQL query to create columns
   const createTableSQL = createColumns(payload);
 
-  // Execute the query to alter the table structure
-  pool.query(createTableSQL, (error, results, fields) => {
-    if (error) {
-      console.error('Error executing query:', error);
-      res.status(500).json({ error: 'Error creating columns' });
-      return;
-    }
+  try {
+    // Execute the query to alter the table structure
+    await db.query(createTableSQL);
     console.log('Table structure altered successfully');
-    
- 
-  });
+    res.status(200).json({ message: 'Columns created successfully' });
+  } catch (error) {
+    console.error('Error creating columns:', error);
+    res.status(500).json({ error: 'Error creating columns' });
+  }
 });
-
